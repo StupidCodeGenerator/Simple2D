0f87ae72b22781c820fc134c908bb75b6c5a6a87
StupidCodeGenerator <13609817879@163.com>
2015-04-13 10:30:22 +0800

    Make the structure simple, get all libs in project folder and manage them

--- src/Main.cpp
+++ src/Main.cpp
@@ -44,7 +44,7 @@ void main(int argc, char **argv) {
 	glEnable(GL_BLEND);
 	tex_2d = SOIL_load_OGL_texture
 		(
-		"fuck.png",
+		"res/fuck.png",
 		SOIL_LOAD_AUTO,
 		SOIL_CREATE_NEW_ID,
 		SOIL_FLAG_MIPMAPS | SOIL_FLAG_INVERT_Y | SOIL_FLAG_NTSC_SAFE_RGB | SOIL_FLAG_COMPRESS_TO_DXT
